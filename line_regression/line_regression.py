305647cba00c86fd3410a13187f34f0ae3eb9626
Homing <470034235@qq.com>
2017-06-12 22:03:44 +0800

    1. add line_regression.py 2. add line_regression.ipynb

---
+++ line_regression/line_regression.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Homing
+@software: PyCharm Community Edition
+@file: line_regression1.py
+@time: 2017/6/12 21:52
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
