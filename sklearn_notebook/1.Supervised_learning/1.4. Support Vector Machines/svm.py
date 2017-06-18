e6938690549ab6e9995d38d6eea6fe01334e21d0
Homing <470034235@qq.com>
2017-06-19 07:14:04 +0800

    add folder 1

---
+++ sklearn_notebook/1.Supervised_learning/1.4. Support Vector Machines/svm.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Homing
+@software: PyCharm Community Edition
+@file: svm.py
+@time: 2017/6/16 21:27
+"""
+
+
+# Support vector machines (SVMs) are a set of supervised learning methods used for classification, regression and outliers detection.
+# 1.4.1. Classification
+
+
+
+
+
+
+
+
+
+
