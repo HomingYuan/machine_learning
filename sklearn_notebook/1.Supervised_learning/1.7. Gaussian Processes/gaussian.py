4e153c78e5eaf831e591c3e23f066dea0e9d02e5
Homing <470034235@qq.com>
2017-06-20 13:48:17 +0800

    Add folder 1.7 Gaussian Processes Add folder 1.8 Cross decomposition Add folder 1.10 Decision Trees

---
+++ sklearn_notebook/1.Supervised_learning/1.7. Gaussian Processes/gaussian.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+web:http://scikit-learn.org/stable/modules/gaussian_process.html
+Gaussian Processes (GP) are a generic supervised learning method designed to
+ solve regression and probabilistic classification problems
+"""
+
+# 1.7.1. Gaussian Process Regression
+# 1.7.2. GPR examples
+# 1.7.2.1. GPR with noise-level estimation
+# 1.7.2.2. Comparison of GPR and Kernel Ridge Regression
+# 1.7.2.3. GPR on Mauna Loa CO2 data
+# 1.7.3. Gaussian Process Classification (GPC)
+# 1.7.4. GPC examples
+# 1.7.4.1. Probabilistic predictions with GPC
+# 1.7.4.2. Illustration of GPC on the XOR dataset
+# 1.7.4.3. Gaussian process classification (GPC) on iris dataset
+# 1.7.5. Kernels for Gaussian Processes
