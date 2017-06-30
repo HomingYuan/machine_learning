69ed1fd35d01194dda8d8cc3fe6f910e275fd689
Homing <470034235@qq.com>
2017-06-30 08:56:36 +0800

    add more folder

---
+++ sklearn_notebook/2.Unsupervised learning/2.1. Gaussian mixture models/Gaussian mixture models.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 2.1. Gaussian mixture models
+# 2.1.1. Gaussian Mixture
+# 2.1.1.1. Pros and cons of class
+# 2.1.1.2. Selecting the number of components in a classical Gaussian Mixture Model
+# 2.1.1.3. Estimation algorithm Expectation-maximization
+# 2.1.2. Variational Bayesian Gaussian Mixture
+# 2.1.2.1. Estimation algorithm: variational inference
+# 2.1.2.2. Pros and cons of variational inference with BayesianGaussianMixture
+# 2.1.2.3. The Dirichlet Process
