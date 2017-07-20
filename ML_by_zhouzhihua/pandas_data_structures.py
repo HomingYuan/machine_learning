0d6af6ec6bec4d769d54d66e65ac1820f938c5ae
Homing <470034235@qq.com>
2017-07-20 17:28:18 +0800

    pandas data

--- ML_by_zhouzhihua/pandas_data_structures.py
+++ ML_by_zhouzhihua/pandas_data_structures.py
@@ -63,3 +63,66 @@ print(pd.DataFrame(data, columns=['C', 'A', 'B']))
 """
 # 8.2.4 From a list of dicts
 data2 = [{'a': 1, 'b': 2}, {'a': 5, 'b': 10, 'c': 20}]
+"""
+print(pd.DataFrame(data2))
+print(pd.DataFrame(data2, index=['first', 'second']))
+print(pd.DataFrame(data2, columns=['a', 'b']))
+"""
+# 8.2.5 From a dict of tuples
+# 8.2.8 Column selection, addition, deletion
+# print(df['one'])
+df['three'] = df['one'] * df['two']
+df['flag'] = df['one'] > 2
+# print(df)
+del df['two']
+three = df.pop('three')
+# print(df)
+df['foo'] = 'bar'
+# print(df)
+df['one_trunc'] = df['one'][:2]
+# print(df)
+df.insert(2, 'bar', df['one']) # location,column_label,values
+# print(df)
+# 8.2.9 Assigning New Columns in Method Chains
+# iris = pd.read_csv('data/iris.data')
+# print(iris.head())
+# 8.2.10 Indexing / Selection
+# print(df.loc['b']) # column label
+# print(df.iloc[2]) # index label
+# 8.2.11 Data alignment and arithmetic
+df = pd.DataFrame(np.random.randn(10, 4), columns=['A', 'B', 'C', 'D'])
+df2 = pd.DataFrame(np.random.randn(7, 3), columns=['A', 'B', 'C'])
+# print(df+df2)
+# print(df - df.iloc[0])
+index = pd.date_range('1/1/2000', periods=8)
+df = pd.DataFrame(np.random.randn(8, 3), index=index, columns=list('ABC'))
+# print(df)
+# 8.2.12 Transposing
+# print(df[:5].T)
+# 8.2.13 DataFrame interoperability with NumPy functions
+# print(np.exp(df))
+# print(np.asarray(df))
+# print(df.T.dot(df))
+s1 = pd.Series(np.arange(5,10))
+"""
+print(s1)
+print(s1.dot(s1))
+"""
+# 8.3 Panel
+# 8.3.1 From 3D ndarray with optional axis labels
+wp = pd.Panel(np.random.randn(2, 5, 4), items=['Item1', 'Item2'],
+ major_axis=pd.date_range('1/1/2000', periods=5),
+ minor_axis=['A', 'B', 'C', 'D'])
+# print(wp)
+# 8.3.2 From dict of DataFrame objects
+data = {'Item1' : pd.DataFrame(np.random.randn(4, 3)),
+ 'Item2' : pd.DataFrame(np.random.randn(4, 2))}
+# print(pd.Panel(data))
+# print(pd.Panel.from_dict(data, orient='minor'))
+df = pd.DataFrame({'a': ['foo', 'bar', 'baz'], 'b': np.random.randn(3)})
+data = {'item1': df, 'item2': df}
+panel = pd.Panel.from_dict(data, orient='minor')
+print(panel['a'])
+print(panel['b'])
+# print(panel['item1']) # 会报错
+# 8.3.3 From DataFrame using to_panel method
