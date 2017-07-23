9761baef67839082d5e622fe3fd9d3af50e0a936
Homing <470034235@qq.com>
2017-07-23 11:33:50 +0800

    update pandas_basic_function

--- ML_by_zhouzhihua/pandas_basic_function.py
+++ ML_by_zhouzhihua/pandas_basic_function.py
@@ -253,5 +253,27 @@ df = pd.DataFrame([['1.1', 2, 3]] * 2, dtype='O')
 # print(df)
 # print(df.apply(pd.to_numeric))
 df = pd.DataFrame([['5us', pd.Timedelta('1day')]] * 2, dtype='O')
-print(df.apply(pd.to_timedelta))
+# print(df.apply(pd.to_timedelta))
 # 9.13.5 gotchas
+# 9.14 Selecting columns based on dtype
+df = pd.DataFrame({'string': list('abc'),
+ 'int64': list(range(1, 4)),
+ 'uint8': np.arange(3, 6).astype('u1'),
+'float64': np.arange(4.0, 7.0),
+
+
+ 'bool1': [True, False, True],
+ 'bool2': [False, True, False],
+ 'dates': pd.date_range('now', periods=3).values,
+ 'category': pd.Series(list("ABC")).astype('category')})
+# print(df)
+df['tdeltas'] = df.dates.diff()
+df['uint64'] = np.arange(3, 6).astype('u8')
+df['other_dates'] = pd.date_range('20130101', periods=3).values
+df['tz_aware_dates'] = pd.date_range('20130101', periods=3, tz='US/Eastern')
+# print(df)
+# print(df.dtypes)
+# print(df.select_dtypes(include=[bool]))
+# print(df.select_dtypes(include=['bool']))
+# print(df.select_dtypes(include=['number', 'bool'], exclude=['unsignedinteger']))
+print(df.select_dtypes(include=['object']))
