e13181b87b71038ae507f11cf2b2ab94bcf06be1
Homing <470034235@qq.com>
2017-07-31 17:26:20 +0800

    modify

--- ML_by_zhouzhihua/data_processing.py
+++ ML_by_zhouzhihua/data_processing.py
@@ -5,9 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_excel('data_process.xlsx')
+df = pd.read_excel('job_data_process.xlsx')
+df[['Low','High']] = df[['Low','High']].astype('float32')
+print(df.dtypes)
 
-df['low'][df['low']==0] = np.mean(df['low'])
-df['high'][df['high']==0] = np.mean(df['high'])
-
-df.to_csv('job_data_process.csv',sep=',')
