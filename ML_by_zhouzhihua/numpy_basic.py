283f6b598abde593773dc4fa66aa103b27fdb7a2
Homing <470034235@qq.com>
2017-08-01 17:13:16 +0800

    update

--- ML_by_zhouzhihua/numpy_basic.py
+++ ML_by_zhouzhihua/numpy_basic.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.linalg import inv, qr
 
 arr1 = np.array([1, 2, 3], dtype=np.float64)
 arr2 = np.array([1, 2, 3], dtype=np.int32)
@@ -63,3 +64,69 @@ x = np.random.randn(8)
 y = np.random.randn(8)
 # print(np.maximum(x, y))
 points = np.arange(-5, 5, 0.01)
+xs, ys = np.meshgrid(points, points)
+# print(ys)
+z = np.sqrt(xs ** 2 + ys ** 2)
+# print(z)
+"""
+plt.imshow(z, cmap=plt.cm.gray)
+plt.colorbar()
+plt.title("Image plot of $\sqrt{x^2 + y^2}$ for a grid of values")
+plt.show()
+"""
+arr = np.random.randn(4, 4)
+# print(np.where(arr > 0, 2, -2))
+# print(np.where(arr > 0, 2, arr))
+arr = np.random.randn(5, 4)
+"""
+print(arr.mean())
+print(np.mean(arr))
+print(arr.sum())
+print(arr.mean(axis=1))
+
+print(arr)
+print(arr.sum(1)) # column direction
+print(arr.sum(0)) # row direction
+"""
+arr = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+"""
+print(arr.cumsum(0)) # cumsum of row
+print(print(arr.cumsum(1))) # cumsum of column
+print(arr.cumsum())
+
+print(arr.cumprod(0))
+print(arr.cumprod(1))
+"""
+arr = np.random.randn(100)
+# print((arr > 0).sum())
+bools = np.array([False, False, True, False])
+"""
+print(bools.any())
+print(bools.all())
+"""
+arr = np.random.randn(8)
+arr.sort()
+# print(arr)
+arr = np.random.randn(5, 3)
+# print(arr)
+arr.sort(1) # sort on column
+# print(arr)
+arr.sort(0)
+# print('sort on row')
+# print(arr)
+large_arr = np.random.randn(1000)
+large_arr.sort()
+# print(large_arr[int(0.05 * len(large_arr))])
+names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])
+# print(np.unique(names))
+ints = np.array([3, 3, 3, 2, 2, 1, 1, 4, 4])
+# print(np.unique(ints))
+x = np.array([[1., 2., 3.], [4., 5., 6.]])
+y = np.array([[6., 23.], [-1, 7], [8, 9]])
+# print(np.dot(x, y))
+X = np.random.randn(5, 5)
+mat = X.T.dot(X)
+# print(inv(mat)) # inv 逆矩阵
+# print(mat.dot(inv(mat)))
+q, r = qr(mat) # QR 分解
+print(q,r)
