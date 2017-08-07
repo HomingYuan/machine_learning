7f6db6cf9d9ba893b9b1efce6bd10852e84a31e9
Homing <470034235@qq.com>
2017-08-07 13:36:15 +0800

    update start with pandas

--- ML_by_zhouzhihua/get_started_with_pandas.py
+++ ML_by_zhouzhihua/get_started_with_pandas.py
@@ -90,3 +90,73 @@ obj3 = Series(['blue', 'purple', 'yellow'], index=[0, 2, 4])
 # print(obj3.reindex(range(6), method='ffill'))
 frame = DataFrame(np.arange(9).reshape((3, 3)), index=['a', 'c', 'd'],
  columns=['Ohio', 'Texas', 'California'])
+# print(frame)
+frame2 = frame.reindex(['a', 'b', 'c', 'd'])
+# print(frame2)
+states = ['Texas', 'Utah', 'California']
+# print(frame.reindex(columns=states))
+# print(frame.reindex(index=['a', 'b', 'c', 'd'], method='ffill',
+ #columns=states))
+# print(frame.ix[['a', 'b', 'c', 'd'], states])
+
+# drop index
+obj = Series(np.arange(5.), index=['a', 'b', 'c', 'd', 'e'])
+new_obj = obj.drop('c')
+# print(new_obj)
+# print(obj.drop(['d', 'c']))
+data = DataFrame(np.arange(16).reshape((4, 4)),
+ index=['Ohio', 'Colorado', 'Utah', 'New York'],
+ columns=['one', 'two', 'three', 'four'])
+# print(data.drop(['Colorado', 'Ohio']))
+# print(data.drop('two', axis=1))
+# print(data.drop('two')) # it will cause error
+# print(data.drop(['two', 'four'], axis=1))
+# Indexing, selection, and filtering
+obj = Series(np.arange(4.), index=['a', 'b', 'c', 'd'])
+"""
+print(obj)
+print(obj['b'])
+print(obj[1])
+print(obj[2:4])
+print(obj[['b', 'a', 'd']])
+print(obj[obj < 2])
+"""
+data = DataFrame(np.arange(16).reshape((4, 4)),
+ index=['Ohio', 'Colorado', 'Utah', 'New York'],
+ columns=['one', 'two', 'three', 'four'])
+# print(data)
+# print(data['two'])
+# print(data.ix['Colorado', ['two', 'three']])
+"""
+print(data.ix[['Colorado', 'Utah'], [3, 0, 1]])
+print(data.ix[2])
+print(data.ix[:'Utah', 'two'])
+print(data.ix[data.three > 5, :3])
+"""
+s1 = Series([7.3, -2.5, 3.4, 1.5], index=['a', 'c', 'd', 'e'])
+s2 = Series([-2.1, 3.6, -1.5, 4, 3.1], index=['a', 'c', 'e', 'f', 'g'])
+# print(s1 + s2)
+df1 = DataFrame(np.arange(9.).reshape((3, 3)), columns=list('bcd'),
+ index=['Ohio', 'Texas', 'Colorado'])
+df2 = DataFrame(np.arange(12.).reshape((4, 3)), columns=list('bde'),
+ index=['Utah', 'Ohio', 'Texas', 'Oregon'])
+# print(df1 + df2)
+df1 = DataFrame(np.arange(12.).reshape((3, 4)), columns=list('abcd'))
+df2 = DataFrame(np.arange(20.).reshape((4, 5)), columns=list('abcde'))
+"""
+print(df1 + df2)
+print(df1.add(df2, fill_value=0))
+print(df1.reindex(columns=df2.columns, fill_value=0))
+"""
+arr = np.arange(12.).reshape((3, 4))
+# print(arr)
+# print(arr - arr[0])
+frame = DataFrame(np.arange(12.).reshape((4, 3)), columns=list('bde'),
+ index=['Utah', 'Ohio', 'Texas', 'Oregon'])
+series = frame.ix[0]
+# print(frame)
+# print(series)
+# print(frame - series)
+series2 = Series(range(3), index=['b', 'e', 'f'])
+# print(frame + series2)
+# Function application and mapping
