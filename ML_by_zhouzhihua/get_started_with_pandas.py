3b6e94ed3132cc4a0c74cf3ddf0f1d800f554446
Homing <470034235@qq.com>
2017-08-07 17:04:38 +0800

    update

--- ML_by_zhouzhihua/get_started_with_pandas.py
+++ ML_by_zhouzhihua/get_started_with_pandas.py
@@ -4,6 +4,8 @@
 from pandas import Series, DataFrame
 import pandas as pd
 import numpy as np
+# import pandas.io.data as web
+# import pandas_datareader.data as web unstable
 
 obj = Series([4, 7, -5, 3])
 """
@@ -160,3 +162,152 @@ series = frame.ix[0]
 series2 = Series(range(3), index=['b', 'e', 'f'])
 # print(frame + series2)
 # Function application and mapping
+frame = DataFrame(np.random.randn(4, 3), columns=list('bde'),
+ index=['Utah', 'Ohio', 'Texas', 'Oregon'])
+# print(frame)
+# print(np.abs(frame))
+f = lambda x: x.max() - x.min()
+# print(frame.apply(f))
+# print(frame.apply(f, axis=1))
+def f(x):
+     return Series([x.min(), x.max()], index=['min', 'max'])
+# print(frame.apply(f))
+format = lambda x: '%.2f' % x
+# print(frame.applymap(format))
+# print(frame['e'].map(format))
+# Sorting and ranking
+obj = Series(range(4), index=['d', 'a', 'b', 'c'])
+# print(obj.sort_index())
+frame = DataFrame(np.arange(8).reshape((2, 4)), index=['three', 'one'],
+ columns=['d', 'a', 'b', 'c'])
+# print(frame.sort_index())
+# print(frame.sort_index(axis=1))
+# print(frame.sort_index(axis=1, ascending=False))
+obj = Series([4, 7, -3, 2])
+# print(obj.order())
+obj = Series([4, np.nan, 7, np.nan, -3, 2])
+# print(obj.order())
+frame = DataFrame({'b': [4, 7, -3, 2], 'a': [0, 1, 0, 1]})
+# print(frame.sort_values(by='b'))
+# print(frame.sort_values(by=['a', 'b']))
+obj = Series([7, -5, 7, 4, 2, 0, 4])
+"""
+print(obj.rank())
+print(obj.rank(method='first'))
+print(obj.rank(ascending=False, method='max'))
+"""
+frame = DataFrame({'b': [4.3, 7, -3, 2], 'a': [0, 1, 0, 1],
+ 'c': [-2, 5, 8, -2.5]})
+# print(frame.rank(axis=1))
+# print(frame.rank())
+# Axis indexes with duplicate values
+obj = Series(range(5), index=['a', 'a', 'b', 'b', 'c'])
+# print(obj)
+# print(obj.index.is_unique)
+# print(obj['a'])
+df = DataFrame(np.random.randn(4, 3), index=['a', 'a', 'b', 'b'])
+# print(df.ix['b'])
+df = DataFrame([[1.4, np.nan], [7.1, -4.5],
+ [np.nan, np.nan], [0.75, -1.3]],
+ index=['a', 'b', 'c', 'd'],
+ columns=['one', 'two'])
+"""
+print(df)
+print(df.sum())
+print(df.sum(axis=1))
+print(df.mean(axis=1, skipna=False))
+print(df.idxmax())
+print(df.cumsum())
+print(df.describe())
+"""
+obj = Series(['a', 'a', 'b', 'c'] * 4)
+# print(obj.describe())
+"""
+all_data = {}
+for ticker in ['AAPL', 'IBM', 'MSFT', 'GOOG']:
+    all_data[ticker] = web.get_data_yahoo(ticker, '1/1/2000', '1/1/2010')
+price = DataFrame({tic: data['Adj Close'] for tic, data in all_data.items()})
+volume = DataFrame({tic: data['Volume']for tic, data in all_data.items()})
+returns = price.pct_change()
+# print(returns.tail())
+# print(returns.MSFT.corr(returns.IBM))
+"""
+
+obj = Series(['c', 'a', 'd', 'a', 'a', 'b', 'b', 'c', 'c'])
+uniques = obj.unique()
+# print(uniques)
+# print(obj.value_counts())
+# print(pd.value_counts(obj.values, sort=False))
+mask = obj.isin(['b', 'c'])
+mask = obj.isin(['b', 'c'])
+# print(mask)
+# print(obj[mask])
+data = DataFrame({'Qu1': [1, 3, 4, 3, 4],
+ 'Qu2': [2, 3, 1, 2, 3],
+ 'Qu3': [1, 5, 2, 4, 4]})
+# print(data)
+result = data.apply(pd.value_counts).fillna(0)
+# print(result)
+# Handling Missing Data
+from numpy import nan as NA
+data = Series([1, NA, 3.5, NA, 7])
+# print(data.dropna())
+# print(data[data.notnull()])
+data = DataFrame([[1., 6.5, 3.], [1., NA, NA],
+ [NA, NA, NA], [NA, 6.5, 3.]])
+cleaned = data.dropna()
+# print(cleaned)
+# print(data.dropna(how='all'))
+# print(data.dropna(how='any'))
+data[4] = NA
+# print(data)
+# print(data.dropna(axis=1, how='all'))
+df = DataFrame(np.random.randn(7, 3))
+df.ix[:4, 1] = NA; df.ix[:2, 2] = NA
+"""
+print(df)
+print(df.dropna(thresh=3))
+print(df.fillna(0))
+print(df.fillna({1: 0.5, 3: -1}))
+"""
+df = DataFrame(np.random.randn(6, 3))
+df.ix[2:, 1] = NA; df.ix[4:, 2] = NA
+# print(df)
+# print(df.fillna(method='ffill'))
+# print(df.fillna(method='ffill', limit=2))
+data = Series([1., NA, 3.5, NA, 7])
+# print(data.fillna(data.mean()))
+data = Series(np.random.randn(10),
+ index=[['a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'd', 'd'],
+ [1, 2, 3, 1, 2, 3, 1, 2, 2, 3]])
+"""
+print(data)
+print(data.index)
+print(data['b'])
+print(data['b':'c'])
+print(data.ix[['b', 'd']])
+print(data[:, 2])
+print(data.unstack())
+print(data.unstack().stack())
+"""
+frame = DataFrame(np.arange(12).reshape((4, 3)),
+ index=[['a', 'a', 'b', 'b'], [1, 2, 1, 2]],
+ columns=[['Ohio', 'Ohio', 'Colorado'],
+ ['Green', 'Red', 'Green']])
+# print(frame)
+frame.index.names = ['key1', 'key2']
+frame.columns.names = ['state', 'color']
+"""
+print(frame)
+print(frame['Ohio'])
+print(frame.swaplevel('key1', 'key2'))
+print(frame.sortlevel(1))
+print(frame.sum(level='key2'))
+print(frame.sum(level='color', axis=1))
+"""
+frame = DataFrame({'a': range(7), 'b': range(7, 0, -1),
+ 'c': ['one', 'one', 'one', 'two', 'two', 'two', 'two'],
+ 'd': [0, 1, 2, 0, 1, 2, 3]})
+print(frame)
+frame2 = frame.set_index(['c', 'd'])
+print(frame2)
